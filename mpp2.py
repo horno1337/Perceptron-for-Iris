e2096f751a40e869bff138f3bb9de01696b2ebda
horno1337 <wiktor.hornowski@gmail.com>
2023-04-03 16:11:08 +0200

    added delta train and predict?

--- mpp2.py
+++ mpp2.py
@@ -16,3 +16,22 @@ def wczytaj_dane(plik):
 # wczytaj dane
 train_data = wczytaj_dane('iris_training.txt')
 test_data = wczytaj_dane('iris_test.txt')
+
+
+def delta_train(training_data, learning_rate, epochs):
+    num_attributes = len(training_data[0][0])
+    weights = [random.uniform(-0.5, 0.5) for _ in range(num_attributes)]
+
+    for _ in range(epochs):
+        for attributes, decision in training_data:
+            output = sum([attributes[i] * weights[i]
+                         for i in range(num_attributes)]) > 0
+            target = decision == 'Iris-setosa'
+            for i in range(num_attributes):
+                weights[i] += learning_rate * (target - output) * attributes[i]
+
+    return weights
+
+
+def predict(weights, attributes):
+    return 'Iris-setosa' if sum([attributes[i] * weights[i] for i in range(len(weights))]) > 0 else 'Iris-versicolor_or_virginica'
